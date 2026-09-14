3b787db06007004308e5d78e226359320b7c79e5
Mustafa <mostafagomaamahmood1999@gmail.com>
2026-09-14 09:49:33 +0300

    feat:add in OrderDAO file two features getAlloOrders for both the Customer and the Admin stii no servlet yet.

--- src/dao/OrderDAO.java
+++ src/dao/OrderDAO.java
@@ -4,10 +4,10 @@ import model.*;
 import util.DBConnection;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderDAO {
     private Connection connection = DBConnection.getConnection();
@@ -53,6 +53,49 @@ public class OrderDAO {
         }
     }
 
+    public List<Order> getAllOrdersToCustomer(Customer customer) throws SQLException {
+        List<Order> orders = new ArrayList<>();
+        connection.setAutoCommit(false);
+        String query = "SELECT order_date,total_amount,status FROM orders WHERE customer_id="+customer.getId();
+        Statement stmt = connection.createStatement();
+        try(ResultSet rs = stmt.executeQuery(query)){
+            while(rs.next()){
+                order.setOrderDate(rs.getDate(1).toLocalDate().atStartOfDay());
+                order.setOrderAmount(rs.getDouble(2));
+                order.setOrderStatus(OrderStatus.valueOf(rs.getString(3)));
+                orders.add(order);
+            }
+            connection.commit();
+        }catch (Exception e){
+            connection.rollback();
+        }
+        return orders;
+    }
+
+    public List<OrderAdminDTO> getAllOrdersToAdmin(Customer customer) throws SQLException {
+        List<OrderAdminDTO> orders = new ArrayList<>();
+        String query = "SELECT * FROM orders " +
+                "join order_items on order.id = order_items.order_id" +
+                "ORDER BY order_date";
+        Statement stmt = connection.createStatement();
+        try(ResultSet rs = stmt.executeQuery(query)){
+            while(rs.next()){
+                ResultSet resultSet = stmt.executeQuery(query);
+                int customerId = rs.getInt("customer_id");
+                int orderId = rs.getInt("order_id");
+                LocalDateTime orderDate = rs.getTimestamp("order_date").toLocalDateTime();
+                double orderAmount = rs.getDouble("order_amount");
+                String orderStatus = rs.getString("order_status");
+                int orderQuantity = rs.getInt("quantity");
+                double price = rs.getDouble("price");
+                orders.add(new OrderAdminDTO(customerId,orderDate,orderAmount,orderStatus,orderId,orderQuantity,price));
+            }
+            connection.commit();
+        }catch (Exception e){
+            connection.rollback();
+        }
+        return orders;
+    }
 //    public int getOrderId() {
 //        return orderId;
 //    }
@@ -74,4 +117,7 @@ public class OrderDAO {
         this.cart = cart;
     }
 
+}
+
+record OrderAdminDTO(int CustomerId,LocalDateTime orderDate,double totalAmount,String orderStatus,int productId,int quantity,double price) {
 }
