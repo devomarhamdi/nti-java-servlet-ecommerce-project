f6edf434738b1c101e730719541a1d6e0224e36c
Eman Ashraf <83442523+EmanAshraf36@users.noreply.github.com>
2026-09-25 19:58:12 +0300

    Completed logout servlet

---
+++ src/servlet/LogoutServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+public class LogoutServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession(false);
+
+        if(session != null){
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath()+"/login?logOut=1");
+    }
+}
